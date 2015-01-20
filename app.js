142f63f01aa4a26f2d99985712c049d97bfd26c3
Brian0610 <brian49345@gmail.com>
2015-01-19 22:54:14 -0500

    added

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ $(document).ready(function(){
 
 	var preload = '<img src="images/preloader.gif" alt="loading..." />'; 
 
-	var loadURL = 'ajax/statement.txt'; 
+	var loadURL = 'ajax/u2.json'; 
 
 	$('button').click(function(){
-		$('.results').html(preload).load(loadURL);  
+		$('.results').html(preload).load(loadURL).append("<p>" + u2[0].name + " is the " + u2.[0].job + " vocalist for U2.</p>");  
 	}); 
 
 }); 
