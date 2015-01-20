c748a15bffb4c9cb856d41c7e0c91e596f9bba1f
Brian0610 <brian49345@gmail.com>
2015-01-19 22:19:05 -0500

    added to app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
 	
+	$.ajaxSetup({
+		cache: false; 
+	}); 
 
+	var preload = '<img src="images/preloader.gif" alt="loading..." />'; 
 
-
+	$('button').click(function(){
+		alert('holy crap, it works!'); 
+	}); 
 
 }); 
