4bef52265108dd5a3186ab5c7e78041348538d00
Brian0610 <brian49345@gmail.com>
2015-01-20 21:50:59 -0500

    added

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 	
 
 	$('button').click(function(){
-		$('.results').html(preload).load(loadURL); 		
+		$('.results').html(preload).load(loadURL + ".groot"); 		
 	}); 
 
 }); 
