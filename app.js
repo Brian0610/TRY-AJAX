fdd98bc01c8adc6eb4b74f2e803c2cd4728fa7bc
Brian0610 <brian49345@gmail.com>
2015-01-21 20:22:27 -0500

    added

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ $(document).ready(function(){
 	$('button').click(function(){
 		$('.results')
 			.html(preload)
-			.load(loadURL, null, function(oranges){
-				alert("Response:\n" + oranges);
+			.load(loadURL, null, function(status){
+				alert("Response:\n" + status);
 			}); 		
 	}); 
 
