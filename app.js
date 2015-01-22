e0c039e7de72ad16dfb3f636493cb931c0353b1c
Brian0610 <brian49345@gmail.com>
2015-01-21 21:01:30 -0500

    added

--- app.js
+++ app.js
@@ -6,19 +6,17 @@ $(document).ready(function(){
 
 	var preload = '<img src="images/preloader.gif" alt="loading..." />'; 
 
-	var jsonURL = 'ajax/bono.json'; 
+	var myScript = 'ajax/script.js'; 
 
 	
 
 	$('button').click(function(){
 		$('.results')
 			.html(preload); 
-			$.getJSON(
-				jsonURL, 
-				null, 
+			$.getScript(
+				myScript, 
 				function(){
-					var singer = "Bono\'s real name is: \n" + bono.name; 
-					$('.results').html(singer); 
+					$('.results').html(myScript); 
 				}
 			); 
 	}); 
